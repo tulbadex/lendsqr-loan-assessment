d342b7dc2bfc08c9dab2e44c6184c8881a0e8bb6
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2023-01-12 05:15:51 -0800

    Amend knexfile to use CLEARDB_DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
 
   development: {
     client: 'mysql',
-    connection: process.env.DATABASE_URL || {
+    connection: process.env.CLEARDB_DATABASE_URL || {
       // filename: './dev.sqlite3'
       database: process.env.DATABASE_NAME,
       user:     process.env.DATABASE_USER,
@@ -25,7 +25,7 @@ module.exports = {
 
   staging: {
     client: 'mysql',
-    connection: process.env.DATABASE_URL || {
+    connection: process.env.CLEARDB_DATABASE_URL || {
       database: process.env.DATABASE_NAME,
       user:     process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD
@@ -41,7 +41,7 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: process.env.DATABASE_URL || {
+    connection: process.env.CLEARDB_DATABASE_URL || {
       database: process.env.DATABASE_NAME,
       user:     process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD
