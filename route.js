ec5624e99298e56ac1372238b90344b489c8f261
Ibrahim Adedayo <ibrahimadedayo@rocketmail.com>
2023-01-12 08:44:05 -0800

    rename readme file

--- route.js
+++ route.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const { createAccount } = require('./Controllers/User');
 const { fundAccount, transferToAccount, withdrawFunds } = require('./Controllers/Transaction');
 
-
 router.post('/create_account', createAccount)
 router.post('/fund_account', fundAccount)
 router.post('/transfer_fund', transferToAccount)
 router.post('/withdraw_fund', withdrawFunds)
-
 
 module.exports = router;
